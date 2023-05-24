cdee05957b188fe5c3afdb76958d7675110363f1
webwizard <webwizard2022@gmail.com>
2023-05-24 07:26:48 +0530

    added admin & front theme

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/user', function () {
+    // return view('welcome');
+    dd(env('APP_DEBUG', 'not found'));
 });
