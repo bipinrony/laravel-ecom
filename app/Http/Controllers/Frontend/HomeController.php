5e035fcc622b50cf11c0ba56153e376e8b05e437
webwizard <webwizard2022@gmail.com>
2023-06-03 08:44:46 +0530

    make frontend categories dynamic

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\CategorySubCategory;
+use App\Models\SubCategory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class HomeController extends Controller
@@ -11,7 +15,26 @@ class HomeController extends Controller
     public function index()
     {
         $data = array();
-        $data['title'] = "Ecom";
+        $data['title'] = "Home";
+        // $categories = DB::table('categories')->where('status', 1)->get();
+        $data['categories'] = $categories = Category::where('status', 1)->get();
+        // foreach ($categories as $category) {
+        //     $subQuery = CategorySubCategory::where('category_id', $category->id)->pluck('sub_category_id')->toArray();
+        //     $subCateData = SubCategory::whereIn(
+        //         'id',
+        //         $subQuery
+        //     )->get();
+
+        //     $category->sub_categories = $subCateData;
+        // }
         return view('frontend.home', $data);
+    }
+
+    public function shop()
+    {
+        $data = array();
+        $data['title'] = "Home";
+        $data['categories'] = Category::where('status', 1)->get();
+        return view('frontend.shop', $data);
     }
 }
