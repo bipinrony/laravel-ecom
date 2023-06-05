af4b6454519f9adc1403ef98713ea2069f20064c
Mamta Kumari <mamtasinghs90@gmail.com>
2023-06-05 06:46:35 +0530

    added product subcategory

--- app/Models/ProductSubCategory.php
+++ app/Models/ProductSubCategory.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class ProductSubCategory extends Model
 {
     use HasFactory;
+    public function subCategory()
+    {
+        return $this->belongsTo(SubCategory::class);
+    }
 }
