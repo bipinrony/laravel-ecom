4488c76c29f39b71908d5e00ab61852c09b90bf1
webwizard <webwizard2022@gmail.com>
2023-06-05 09:09:31 +0530

    05-06-2023

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -13,7 +13,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
 class ProductController extends Controller
-{  
+{
     public function index()
     {
         $data = array();
@@ -29,53 +29,52 @@ class ProductController extends Controller
         $data['title'] = "Product";
         $data['categories'] = Category::all();
         $data['subcategories'] = SubCategory::all();
-        return view('admin.product.create', $data); 
+        return view('admin.product.create', $data);
     }
 
     // Store data
     public function store(Request $request)
     {
         $request->validate([
-             'name'=>'required|unique:products,name',
-             'category_id'=>'required|array',
-             'sub_category_id'=>'required|array',
-             'image'=>'nullable|image',
+            'name' => 'required|unique:products,name',
+            'category_id' => 'required|array',
+            'sub_category_id' => 'required|array',
+            'image' => 'nullable|image',
         ]);
 
         $product = new Product();
         $product->name = $request->name;
         $product->slug = Str::slug($request->name, '_');
         $product->price = $request->price;
-        $product->sale_price = $request->sale_price ;
+        $product->sale_price = $request->sale_price;
         $product->qty_available = $request->qty_available;
         $product->short_description = $request->short_description;
         $product->description = $request->description;
         $product->status = $request->status;
 
-        if($product->save()){
-            foreach($request->category_id as $cat){
-                $productcategory = new ProductCategory();
-                $productcategory->category_id = $cat;
-                $productcategory->product_id = $product->id;
-                $productcategory->save();
+        if ($product->save()) {
+            foreach ($request->category_id as $cat) {
+                $productCategory = new ProductCategory();
+                $productCategory->category_id = $cat;
+                $productCategory->product_id = $product->id;
+                $productCategory->save();
             }
 
-            foreach($request->sub_category_id as $subcat){
-                $productsubcat = new ProductSubCategory();
-                $productsubcat->sub_category_id = $subcat;
-                $productsubcat->product_id = $product->id;
-                $productsubcat->save();
+            foreach ($request->sub_category_id as $subcat) {
+                $productSubcategory = new ProductSubCategory();
+                $productSubcategory->sub_category_id = $subcat;
+                $productSubcategory->product_id = $product->id;
+                $productSubcategory->save();
             }
-            
-           
-            if($files=$request->file('images')){
-                foreach($files as $file){
-                    $productimg = new ProductImage();
+
+
+            if ($files = $request->file('images')) {
+                foreach ($files as $file) {
+                    $productImg = new ProductImage();
                     $path = $file->store('public/images/products');
-                    $productimg->product_image =$path;
-                    $productimg->product_id = $product->id;
-                    $productimg->save();
-
+                    $productImg->product_image = $path;
+                    $productImg->product_id = $product->id;
+                    $productImg->save();
                 }
             }
             return redirect()->route('admin.products')->with('success', 'Product added successfully');
@@ -87,16 +86,14 @@ class ProductController extends Controller
     //delete
     public function delete(Product $product)
     {
-        $productid = $product->id;
-        if(!empty($product) && $product->delete()){
-            ProductCategory::where('product_id', $productid)->delete();
-            ProductSubCategory::where('product_id', $productid)->delete();
-            ProductImage::where('product_id', $productid)->delete();
+        $productId = $product->id;
+        if (!empty($product) && $product->delete()) {
+            ProductCategory::where('product_id', $productId)->delete();
+            ProductSubCategory::where('product_id', $productId)->delete();
+            ProductImage::where('product_id', $productId)->delete();
             return redirect()->route('admin.products')->with('success', 'Product deleted successfully');
         } else {
             return redirect()->route('admin.products')->with('error', 'Something went wrong');
         }
     }
 }
-
-
