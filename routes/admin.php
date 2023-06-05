2fff2d2a525309316b4e6d66d9c2084b420f7c75
Mamta Kumari <mamtasinghs90@gmail.com>
2023-06-05 06:54:41 +0530

    added  product route

--- routes/admin.php
+++ routes/admin.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\SubCategoryController;
+use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,23 +23,34 @@ Route::post('admin-login', [AuthController::class, 'login'])->name('admin.login.
 
 Route::prefix('admin')->middleware(['auth', 'admin'])->group(function () {
     Route::get('dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
-
+   // Category
     Route::get('categories', [CategoryController::class, 'index'])->name('admin.categories');
     Route::get('categories/add', [CategoryController::class, 'create'])->name('admin.categories.get');
-    Route::post('categories/add', [CategoryController::class, 'store'])->name('admin.categories.post');
+    Route::post('categories/store', [CategoryController::class, 'store'])->name('admin.categories.post');
 
     Route::get('categories/delete/{category}', [CategoryController::class, 'delete'])->name('admin.categories.delete');
     Route::get('categories/edit/{category}', [CategoryController::class, 'edit'])->name('admin.categories.edit');
     Route::post('categories/update', [CategoryController::class, 'update'])->name('admin.categories.update');
-
+    
+    // Subcategory
     Route::get('sub-categories', [SubCategoryController::class, 'index'])->name('admin.sub_categories');
     Route::get('subcategories/add', [SubCategoryController::class, 'create'])->name('admin.subcategories.get');
-
-    Route::post('subcategories/add', [SubCategoryController::class, 'store'])
+    Route::post('subcategories/store', [SubCategoryController::class, 'store'])
         ->name('admin.subcategories.post');
 
     Route::get('subcategories/edit/{subCategory}', [SubCategoryController::class, 'edit'])
         ->name('admin.subcategories.edit');
     Route::post('subcategories/update', [SubCategoryController::class, 'update'])->name('admin.subcategories.update');
     Route::get('subcategories/delete/{subcategory}', [SubCategoryController::class, 'delete'])->name('admin.subcategories.delete');
+
+    // Product
+    Route::get('product', [ProductController::class, 'index'])->name('admin.products');
+    Route::get('product/add', [ProductController::class, 'create'])->name('admin.product.get');
+    Route::post('product/store', [ProductController::class, 'store'])->name('admin.product.post');
+
+    Route::get('product/delete/{product}', [ProductController::class, 'delete'])->name('admin.product.delete');
+    Route::get('product/edit/{product}', [ProductController::class, 'edit'])->name('admin.product.edit');
+    Route::post('product/update', [ProductController::class,'update'])->name('admin.product.update');
+
+    
 });
