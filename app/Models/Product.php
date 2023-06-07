df55f40f35c8a0977700a32ea3a66b2366ff9805
Mamta Kumari <mamtasinghs90@gmail.com>
2023-06-07 07:08:17 +0530

    add mutator and accessor

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
 
 class Product extends Model
 {
@@ -24,6 +25,32 @@ class Product extends Model
         return $this->hasMany(ProductImage::class);
     }
 
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => ucwords($value),
+            set: fn (string $value) => ucwords($value),
+        );
+    }
 
+    protected function shortDescription(): Attribute
+    {
+        return Attribute::make(
+           get: fn (string $value)=> ucwords($value),
+           set: fn (string $value)=> ucwords($value), 
+        );
+    }
+
+    protected function description(): Attribute
+    {
+        return Attribute::make(
+         get: fn (string $value)=> ucwords($value),
+         set: fn (string $value)=> ucwords($value),
+
+        ); 
+        $user = Category::find(1);
+    }
+
+    
 }
 
