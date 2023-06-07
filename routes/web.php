861db3f2fac94912202d6a18cd4d1726f3914c1d
Mamta Kumari <mamtasinghs90@gmail.com>
2023-06-07 07:13:58 +0530

    add login and register route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/shop/{category_slug}/{sub_category_slug?}', [HomeController::class, 'shop'])->name('shop');
+Route::get('/login', [HomeController::class, 'login'])->name('login');
+Route::get('/register', [HomeController::class, 'register'])->name('register');
