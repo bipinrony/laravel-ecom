0f686a32e6031777a80116db117bc2d2987753cf
Mamta Kumari <mamtasinghs90@gmail.com>
2023-06-11 22:27:57 +0530

    make slider dynamic

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -4,18 +4,19 @@
 @section('header-carousel')
     <div id="header-carousel" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
-            <div class="carousel-item active" style="height: 410px;">
-                <img class="img-fluid" src="{{ url('front/img/carousel-1.jpg') }}" alt="Image">
+            @foreach($slider as $slider)
+            <div class="carousel-item {{ $loop->first ? 'active' : ''}}" style="height: 410px;">
+                <img class="img-fluid" src="{{ Storage::url($slider->image) }}" alt="Image">
                 <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                     <div class="p-3" style="max-width: 700px;">
-                        <h4 class="text-light text-uppercase font-weight-medium mb-3">10% Off Your First
-                            Order</h4>
-                        <h3 class="display-4 text-white font-weight-semi-bold mb-4">Fashionable Dress</h3>
+                        <h4 class="text-light text-uppercase font-weight-medium mb-3">{{ $slider->para_text }}</h4>
+                        <h3 class="display-4 text-white font-weight-semi-bold mb-4">{{ $slider->head_text }}</h3>
                         <a href="" class="btn btn-light py-2 px-3">Shop Now</a>
                     </div>
                 </div>
             </div>
-            <div class="carousel-item" style="height: 410px;">
+            @endforeach
+            {{-- <div class="carousel-item" style="height: 410px;">
                 <img class="img-fluid" src="{{ url('front/img/carousel-2.jpg') }}" alt="Image">
                 <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                     <div class="p-3" style="max-width: 700px;">
@@ -25,7 +26,7 @@
                         <a href="" class="btn btn-light py-2 px-3">Shop Now</a>
                     </div>
                 </div>
-            </div>
+            </div> --}}
         </div>
         <a class="carousel-control-prev" href="#header-carousel" data-slide="prev">
             <div class="btn btn-dark" style="width: 45px; height: 45px;">
@@ -85,15 +86,17 @@
                     <h5 class="font-weight-semi-bold m-0">Men's dresses</h5>
                 </div>
             </div>
+            {{-- @foreach($products as $product)
             <div class="col-lg-4 col-md-6 pb-1">
                 <div class="cat-item d-flex flex-column border mb-4" style="padding: 30px;">
-                    <p class="text-right">15 Products</p>
+                    <p class="text-right">{{ $product->subCategory->SubCategory-> }}</p>
                     <a href="" class="cat-img position-relative overflow-hidden mb-3">
                         <img class="img-fluid" src="{{ url('front/img/cat-2.jpg') }}" alt="">
                     </a>
                     <h5 class="font-weight-semi-bold m-0">Women's dresses</h5>
                 </div>
             </div>
+            @endforeach --}}
             <div class="col-lg-4 col-md-6 pb-1">
                 <div class="cat-item d-flex flex-column border mb-4" style="padding: 30px;">
                     <p class="text-right">15 Products</p>
